81931869fa5bf5dded8c6d18029c43db198bf55d
Matthew Altus <matt@altus.id.au>
2015-03-06 22:21:33 +1030

    Add area and stack options to the data output.

--- lib/Muninicious/Munin/RRD/Data.pm
+++ lib/Muninicious/Munin/RRD/Data.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use Mojo::Base -base;
 
+use Mojo::JSON qw/true false/;
+
 use Muninicious::Munin::RRD::Colours;
 
 use Math::BigFloat;
@@ -140,6 +142,8 @@ sub get_data {
     $field_data->{'label'}  = $field->metadata('label');
     $field_data->{'info'}   = $field->metadata('info');
     $field_data->{'colour'} = $self->colours->get_field_colour($field);
+    $field_data->{'area'}   = ($field->metadata('draw') || '') =~ /AREA/ ? true : false;
+    $field_data->{'stack'}  = ($field->metadata('draw') || '') =~ /STACK/ ? true : false;
     $field_data->{'data'}   = [];
     foreach my $clock (sort keys %{$values{'MIN'}}) {
       next if ($clock > $now);
